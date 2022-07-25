3e9eb25731b690b3f86ea259061cf71d7fe8772a
Navashakthi Gnansekaran <46438723+Navashakthi@users.noreply.github.com>
2022-07-25 13:36:59 +0530

    Add files via upload

---
+++ InvoiceMicroAPP/invoice/invoice.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import json
+from werkzeug.exceptions import NotFound
+
+app = Flask(__name__)
+
+invoicefile = "database/invoice.json"
+with open(invoicefile, "r") as f:
+    invoices = json.load(f)
+
+@app.route("/", methods=['GET'])
+def hello():
+    return ({
+        "uri": "/",
+        "subresource_uris": {
+            "invoice": "/invoice",
+            "invoices": "/invoices/Invoice_Number"
+        }
+    })
+
+
+@app.route("/invoice", methods=['GET'])
+def invoice_list():
+    return (invoices)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
